41b416ec17e3adc1f0fdf71d8f4823355cef12cb
조성준 <39251873+dlddu@users.noreply.github.com>
2022-12-19 21:59:15 +0900

    Time: 533 ms (95.56%), Space: 76.2 MB (83.70%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cs
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cs
@@ -1,5 +1,6 @@
 public class Solution {
     public bool ValidPath(int n, int[][] edges, int source, int destination) {
+        // 출발지와 목적지가 동일
         if (source == destination) return true;
         
         var map = new List<int>[n];
@@ -13,30 +14,34 @@ public class Solution {
             map[b].Add(a);
         }
         
+        // 출발지에 연결된 그래프가 없음
         if (map[source] == null) return false;
 
+        // 방문 여부 기록
         var visited = new bool[n];
+        
+        // 스택으로 DFS
         var current = new Stack<int>();
-        foreach (var node in map[source])
-        {
-            if (node == destination) return true;
-            current.Push(node);
-        }
+        current.PushRange(map[source]);
 
         while (current.TryPop(out var candidate))
         {
-            if (candidate == destination) return true;
-            if (map[candidate] == null) return false;
-            if (visited[candidate]) continue;
+            if (candidate == destination) return true; // 도착
+            if (map[candidate] == null) return false; // 진행 불가능
+            if (visited[candidate]) continue; // 이미 방문
+            
             visited[candidate] = true;
-
-            foreach (var node in map[candidate])
-            {
-                if (node == destination) return true;
-                current.Push(node);
-            }
+            current.PushRange(map[candidate]);
         }
 
         return false;
+    }
+}
+
+internal static class Extension
+{
+    internal static void PushRange(this Stack<int> a, List<int> b)
+    {
+        foreach (var n in b) a.Push(n);
     }
 }
