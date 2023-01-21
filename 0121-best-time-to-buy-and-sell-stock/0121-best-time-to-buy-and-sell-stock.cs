c55cf1023f81afa8edc0cda341cd8ec0baeabc8b
조성준 <39251873+dlddu@users.noreply.github.com>
2023-01-20 17:45:40 -0800

    Time: 217 ms (99.01%), Space: 49.6 MB (14.38%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cs
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cs
@@ -1,10 +1,15 @@
 public class Solution {
     public int MaxProfit(int[] prices) {
         var dp = new int[prices.Length];
+        var max = 0;
         
         for (var i = 1; i < prices.Length; i++)
+        {
             dp[i] = Math.Max(dp[i-1] + prices[i] - prices[i-1], 0);
+            if (dp[i] > max)
+                max = dp[i];
+        }
         
-        return dp.Max();
+        return max;
     }
 }
