7a0b30227fef3ef5eebe65702ce0f6bfb05a5493
조성준 <39251873+dlddu@users.noreply.github.com>
2023-01-22 00:01:09 -0800

    Time: 98 ms (49.80%), Space: 39.7 MB (22.82%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cs
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cs
@@ -1,29 +1,14 @@
 public class Solution {
-    private int[] _cost;
-    private int[] dp;
-    
     public int MinCostClimbingStairs(int[] cost) {
         var len = cost.Length;
         
-        _cost = cost.Concat(new int[] { 0 }).ToArray();
-        dp = new int[len];
-        for (var i = 0; i < len; i++)
-            dp[i] = -1;
-        
+        var dp = new int[len];
         dp[0] = cost[0];
         dp[1] = cost[1];
         
-        return cal(len);
-    }
-    
-    private int cal(int n)
-    {
-        if (dp[n-2] < 0)
-            dp[n-2] = cal(n-2);
+        for (var i = 2; i < len; i++)
+            dp[i] = cost[i] + Math.Min(dp[i-2], dp[i-1]);
         
-        if (dp[n-1] < 0)
-            dp[n-1] = cal(n-1);
-        
-        return Math.Min(dp[n-1], dp[n-2]) + _cost[n];
+        return Math.Min(dp[len-1], dp[len-2]);
     }
 }
