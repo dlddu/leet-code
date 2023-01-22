e8edaa6564dbc988bb6dc8e1647332427dc9a260
조성준 <39251873+dlddu@users.noreply.github.com>
2023-01-22 00:04:45 -0800

    Time: 182 ms (5.08%), Space: 37.5 MB (96.74%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cs
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cs
@@ -2,13 +2,16 @@ public class Solution {
     public int MinCostClimbingStairs(int[] cost) {
         var len = cost.Length;
         
-        var dp = new int[len];
-        dp[0] = cost[0];
-        dp[1] = cost[1];
+        var downdown = cost[0];
+        var down = cost[1];
         
         for (var i = 2; i < len; i++)
-            dp[i] = cost[i] + Math.Min(dp[i-2], dp[i-1]);
+        {
+            var next = cost[i] + Math.Min(downdown, down);
+            downdown = down;
+            down = next;
+        }
         
-        return Math.Min(dp[len-1], dp[len-2]);
+        return Math.Min(downdown, down);
     }
 }
