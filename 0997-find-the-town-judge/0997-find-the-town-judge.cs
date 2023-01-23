51a35d297235af38dd4e091c4d68783728e8be8a
조성준 <39251873+dlddu@users.noreply.github.com>
2023-01-23 03:23:07 -0800

    Time: 267 ms (60.00%), Space: 54.5 MB (30.71%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cs
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cs
@@ -4,7 +4,7 @@ public class Solution {
             return trust.Any() ? -1 : 1;
         
         var dict = new Dictionary<int, List<int>>();
-        var candidate = Enumerable.Range(0, n).ToList();
+        var notCand = new bool[n];
         
         foreach (var t in trust)
         {
@@ -12,14 +12,14 @@ public class Solution {
                 dict[t[1]] = new List<int>();
             dict[t[1]].Add(t[0]);
             
-            candidate.Remove(t[0] - 1);
+            notCand[t[0] - 1] = true;
         }
         
         var mostTrusted = dict.Where(kv => kv.Value.Count() == n - 1);
         
         if (mostTrusted.Count() != 1) return -1;
         var maybe = mostTrusted.Single().Key;
-        if (!candidate.Contains(maybe - 1)) return -1;
+        if (notCand[maybe - 1]) return -1;
         
         return maybe;
     }
