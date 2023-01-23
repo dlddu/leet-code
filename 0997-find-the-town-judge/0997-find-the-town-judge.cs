c33aafa23af2aa87612274569c8ddf144dd34bfd
조성준 <39251873+dlddu@users.noreply.github.com>
2023-01-23 03:21:30 -0800

    Time: 243 ms (93.57%), Space: 59.4 MB (7.14%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cs
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cs
@@ -1,30 +1,26 @@
 public class Solution {
     public int FindJudge(int n, int[][] trust) {
         if (n == 1)
-        {
-            if (trust.Any())
-                return -1;
-            return 1;
-        }
-        if (!trust.Any()) return -1;
+            return trust.Any() ? -1 : 1;
         
         var dict = new Dictionary<int, List<int>>();
+        var candidate = Enumerable.Range(0, n).ToList();
         
         foreach (var t in trust)
         {
-            if (!dict.ContainsKey(t[0]))
-                dict[t[0]] = new List<int>();
-            dict[t[0]].Add(t[1]);
+            if (!dict.ContainsKey(t[1]))
+                dict[t[1]] = new List<int>();
+            dict[t[1]].Add(t[0]);
+            
+            candidate.Remove(t[0] - 1);
         }
         
-        if (dict.Count() != n - 1) return -1;
+        var mostTrusted = dict.Where(kv => kv.Value.Count() == n - 1);
         
-        IEnumerable<int> inter = dict[trust[0][0]];
-        foreach (var (key, value) in dict)
-            inter = inter.Intersect(value);
+        if (mostTrusted.Count() != 1) return -1;
+        var maybe = mostTrusted.Single().Key;
+        if (!candidate.Contains(maybe - 1)) return -1;
         
-        if (inter.Count() == 1)
-            return inter.First();
-        return -1;
+        return maybe;
     }
 }
