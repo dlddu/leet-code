891cbda3fb273ee333340a3f8ed903567936ffa9
조성준 <39251873+dlddu@users.noreply.github.com>
2023-01-23 00:36:15 -0800

    Time: 34 ms (23.44%), Space: 25.2 MB (93.75%) - LeetHub

--- 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.cs
+++ 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.cs
@@ -7,14 +7,18 @@ public class Solution {
         dp[0] = 0;
         dp[1] = 1;
         
+        var max = 1;
+        
         for (var i = 2; i <= n; i++)
         {
             if (i % 2 == 0)
                 dp[i] = dp[i / 2];
             else
                 dp[i] = dp[(i - 1) / 2] + dp[(i - 1) / 2 + 1];
+            
+            max = Math.Max(max, dp[i]);
         }
         
-        return dp.Max();
+        return max;
     }
 }
