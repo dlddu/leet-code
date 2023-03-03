66b7f609c2e384b3f9f7955b38553479e018af16
조성준 <39251873+dlddu@users.noreply.github.com>
2023-03-03 10:14:47 +0900

    Time: 62 ms (77.21%), Space: 36.3 MB (68.96%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cs
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cs
@@ -1,5 +1,14 @@
 public class Solution {
     public int StrStr(string haystack, string needle) {
-        return haystack.IndexOf(needle);
+        for (var i = 0; i < haystack.Length - needle.Length + 1; i++)
+            if (haystack[i] == needle[0] && isWord(haystack, needle, i)) return i;
+        return -1;
+    }
+    
+    private bool isWord(string haystack, string needle, int i)
+    {
+        for (var j = 0; j < needle.Length; j++)
+            if (haystack[i+j] != needle[j]) return false;
+        return true;
     }
 }
